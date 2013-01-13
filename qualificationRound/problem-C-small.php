7cde376fe1b4547e5a64393c7ef1f33a2fe7aefb
Morio Fukuda <morio_fukuda@voyagegroup.com>
2013-01-13 14:23:31 +0900

    Round1 A 終了。

--- qualificationRound/problem-C-small.php
+++ qualificationRound/problem-C-small.php
@@ -22,7 +22,7 @@ while($loopLimit >= $loopCounter){
 
 		$rider = 0;			//　ジェットコースターに何人乗ったかを格納
 
-		if($groupPos == $n){
+		if($groupPos === $n){
 			$groupPos = 0;
 		}
 
@@ -35,7 +35,7 @@ while($loopLimit >= $loopCounter){
 			// 最初に乗ったグループは、グループリストの途中のグループで、一番最後のグループが乗車したら
 			// 1回の乗車で全グループが乗れた場合は、グループリストの先頭グループを乗車させるのではなく、発車してほしい。
 			// グループリストの途中からの
-			if($groupPos == $n && $startPos != 0){
+			if($groupPos === $n && $startPos != 0){
 				$groupPos = 0;
 				$rideCounter++;
 			}
